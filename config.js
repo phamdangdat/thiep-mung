cae806f65705c723fa33bd753f8aca99ddc63065
phamdangdat <118593229+phamdangdat@users.noreply.github.com>
2022-11-19 23:30:29 +0700

    Add files via upload
    Signed-off-by: phamdangdat <118593229+phamdangdat@users.noreply.github.com>

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Mai';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Happy birthday';
+const titleCard = 'Tặng bạn tớ';
+const contentCard = 'Happy Birthday bạn của tớ chúc cậu sang tuổi mới nhiều sức khỏe, ngày càng xinh gái hơn học thật giỏi, tương lai lấy chồng giàu và mãi là Mai-đẹt-ti-ni của chúng tớ💘';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'luunguyetmai.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
