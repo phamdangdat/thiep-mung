8a7a8a0012ca1310b704403ff18d04f21a6884b6
phamdangdat <118593229+phamdangdat@users.noreply.github.com>
2022-11-19 23:41:07 +0700

    Add files via upload
    Signed-off-by: phamdangdat <118593229+phamdangdat@users.noreply.github.com>

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Mai';
+const giftUrl = 'https://scontent.fhph1-2.fna.fbcdn.net/v/t1.15752-9/311948245_5355829034528742_8389893547035102733_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=ae9488&_nc_ohc=NqfwggFfOdgAX8r3X3K&_nc_ht=scontent.fhph1-2.fna&oh=03_AdRUEGzqBMEaIozG1frl8zFgX7T0MkrjKALAUE-JmXvV1Q&oe=63A08D64';
+const eventName = 'Happy birthday';
+const titleCard = 'Tặng bạn tớ';
+const contentCard = 'Happy Birthday bạn của tớ chúc cậu sang tuổi mới nhiều sức khỏe, ngày càng xinh gái hơn học thật giỏi, tương lai lấy chồng giàu và mãi là Mai-đẹt-ti-ni của chúng tớ💘';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'luunguyetmai.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
